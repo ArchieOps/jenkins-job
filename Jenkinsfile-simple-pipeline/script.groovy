a0d025d624af1daaeab4f16e73d762f59002288a
Bhavesh Muleva <bhaveshmuleva503@gmail.com>
2022-12-14 14:06:39 +0000

    Update script.groovy

--- Jenkinsfile-simple-pipeline/script.groovy
+++ Jenkinsfile-simple-pipeline/script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'b8306909-6521-4e9a-9825-69e4c47f15aa', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t bhaveshmuleva/java-demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push nanajanashia/java-demo-app:jma-2.0'
     }
 } 
 
