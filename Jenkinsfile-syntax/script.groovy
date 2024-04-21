1b84b5f2b5bc4ff70d8b3f70ed17184f8d0f7731
Ardo <61459987+ArchieOps@users.noreply.github.com>
2024-04-21 18:06:35 +0700

    Update script.groovy

--- Jenkinsfile-syntax/script.groovy
+++ Jenkinsfile-syntax/script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t archieops/my-private-repo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push archieops/my-private-repo:jma-2.0'
     }
 } 
 
